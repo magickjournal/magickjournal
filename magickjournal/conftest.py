c11768485d7e91b23bc8c001f86165ea40905704
Brian Choate <brianc@andor-mbp13-2020.lan>
2021-12-18 03:31:00 -0800

    Init

---
+++ magickjournal/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from magickjournal.users.models import User
+from magickjournal.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
